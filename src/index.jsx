3b2a597412597333d64fb83de7049595e8c4a2d9
Avadhut Prabhudesai <avadhutprabhudesai23@gmail.com>
2022-07-07 15:50:32 +0530

    added support for jest.

--- src/index.jsx
+++ src/index.jsx
@@ -1,1 +1,8 @@
-console.log('Vite ReactJS');
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+export default function App() {
+  return <h1>App</h1>;
+}
+
+ReactDOM.render(<App />, document.getElementById('root'));
